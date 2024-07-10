1b5beb02cd4adcdb368871af79f5d2ae910cd34f
oralxan <oralxan@users.noreply.github.com>
2024-07-10 23:47:32 +0500

    first commit

--- src/settings/base.py
+++ src/settings/base.py
@@ -3,11 +3,11 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 SECRET_KEY = 'django-insecure-%q786d6m1sq#tvu=4arnus&jm3dbi4z3o_&!v*jrj^rjy7p787'
-DEBUG = True
+
 
 
 
@@ -24,7 +24,7 @@ CONTACT_EMAIL = 'orallxannajmatdinova@gmail.com'
 
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -73,12 +73,7 @@ WSGI_APPLICATION = 'src.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
