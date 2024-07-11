b2eb4eb96e9c0ca3f962f5020e1fa9aed337b449
oralxan <oralxan@users.noreply.github.com>
2024-07-11 20:08:22 +0500

    J

--- src/base.py
+++ src/base.py
@@ -70,8 +70,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'src.wsgi.application'
 
 
+
+
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 
--- src/settings/production.py
+++
@@ -1,8 +0,0 @@
-from .base import *
-DEBUG = False
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'production.sqlite3',
-    }
-}
